d4644417e2433c9b45f47dc0a5817b1773468b42
beherevirtually <88733026+beherevirtually@users.noreply.github.com>
2021-08-10 20:14:08 -0600

    Update index.js

--- src/templates/ProductPage/index.js
+++ src/templates/ProductPage/index.js
@@ -12,78 +12,143 @@ import {
 } from '~/utils/styles'
 import { ProductTitle, ProductDescription } from './styles'
 
-const ProductPage = ({ data }) => {
-  const product = data.shopifyProduct
+const ProductPage = ({ data: { shopifyProduct: product } }) => {
+  const colors = product.options.find(
+    option => option.name.toLowerCase() === "color"
+  ).values
+  const sizes = product.options.find(
+    option => option.name.toLowerCase() === "size"
+  ).values
+
+  const variants = useMemo(() => prepareVariantsWithOptions(product.variants), [
+    product.variants,
+  ])
+  const images = useMemo(() => prepareVariantsImages(variants, "color"), [
+    variants,
+  ])
+
+  if (images.length < 1) {
+    throw new Error("Must have at least one product image!")
+  }
+
+  const addItemToCart = useAddItemToCart()
+  const [variant, setVariant] = useState(variants[0])
+  const [color, setColor] = useState(variant.color)
+  const [size, setSize] = useState(variant.size)
+  const [addedToCartMessage, setAddedToCartMessage] = useState(null)
+
+  useEffect(() => {
+    const newVariant = variants.find(variant => {
+      return variant.size === size && variant.color === color
+    })
+
+    if (variant.shopifyId !== newVariant.shopifyId) {
+      setVariant(newVariant)
+    }
+  }, [size, color, variants, variant.shopifyId])
+
+  const gallery =
+    images.length > 1 ? (
+      <Grid gap={2} columns={6}>
+        {images.map(({ src, color }) => (
+          <Thumbnail key={color} src={src} onClick={() => setColor(color)} />
+        ))}
+      </Grid>
+    ) : null
+
+  function handleAddToCart() {
+    addItemToCart(variant.shopifyId, 1)
+    setAddedToCartMessage("🛒 Added to your cart!")
+  }
+
   return (
-    <>
-      <SEO title={product.title} description={product.description} />
-      <Container>
-        <TwoColumnGrid>
-          <GridLeft>
-            {product.images.map(image => (
-              <Img
-                fluid={image.localFile.childImageSharp.fluid}
-                key={image.id}
-                alt={product.title}
+    <Layout>
+      <SEO title={product.title} />
+      {addedToCartMessage ? (
+        <Alert sx={{ mb: 4 }} variant="primary">
+          {addedToCartMessage}
+          <Close
+            ml="auto"
+            mr={-2}
+            sx={{
+              "&:hover": {
+                cursor: "pointer",
+              },
+            }}
+            onClick={() => setAddedToCartMessage(null)}
+          />
+        </Alert>
+      ) : null}
+      <Grid gap={4} columns={2}>
+        <div>
+          <div
+            sx={{
+              border: "1px solid gray",
+              padding: 2,
+              marginBottom: 2,
+            }}
+          >
+            <Img fluid={variant.image.localFile.childImageSharp.fluid} />
+          </div>
+          {gallery}
+        </div>
+        <div sx={{ display: "flex", flexDirection: "column" }}>
+          <Styled.h1 sx={{ mt: 0, mb: 2 }}>{product.title}</Styled.h1>
+          <div dangerouslySetInnerHTML={{ __html: product.descriptionHtml }} />
+          <div>
+            <Grid padding={2} columns={2}>
+              <OptionPicker
+                key="Color"
+                name="Color"
+                options={colors}
+                selected={color}
+                onChange={event => setColor(event.target.value)}
               />
-            ))}
-          </GridLeft>
-          <GridRight>
-            <ProductTitle>{product.title}</ProductTitle>
-            <ProductDescription
-              dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
-            />
-            <ProductForm product={product} />
-          </GridRight>
-        </TwoColumnGrid>
-      </Container>
-    </>
+              <OptionPicker
+                key="Size"
+                name="Size"
+                options={sizes}
+                selected={size}
+                onChange={event => setSize(event.target.value)}
+              />
+            </Grid>
+          </div>
+          <Button
+            sx={{ margin: 2, display: "block" }}
+            onClick={handleAddToCart}
+          >
+            Add to Cart
+          </Button>
+        </div>
+      </Grid>
+    </Layout>
   )
 }
 
-export const query = graphql`
-  query($handle: String!) {
-    shopifyProduct(handle: { eq: $handle }) {
+export default ProductPage
+
+export const ProductPageQuery = graphql`
+  query productPage($productId: String!) {
+    shopifyProduct(id: { eq: $productId }) {
       id
       title
-      handle
-      productType
-      description
       descriptionHtml
-      shopifyId
       options {
-        id
         name
         values
       }
       variants {
-        id
-        title
-        price
-        availableForSale
         shopifyId
         selectedOptions {
           name
           value
         }
-      }
-      priceRange {
-        minVariantPrice {
-          amount
-          currencyCode
-        }
-        maxVariantPrice {
-          amount
-          currencyCode
-        }
-      }
-      images {
-        originalSrc
-        id
-        localFile {
-          childImageSharp {
-            fluid(maxWidth: 910) {
-              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        image {
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 446) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
             }
           }
         }
@@ -91,5 +156,3 @@ export const query = graphql`
     }
   }
 `
-
-export default ProductPage
